c77223e4569a6ca369a65affe4724eb67b0cf337
Diogo Martins <diogo.martins@it-objects.pt>
2025-03-12 20:28:24 +0000

    Minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppSideNavComponent } from './components/app-side-nav/app-side-nav.component';
+import { AuthService } from './services';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, AppHeaderComponent, AppSideNavComponent],
+    imports: [RouterOutlet, AppHeaderComponent, AppSideNavComponent, LoginComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+    private authService = inject(AuthService);
+    public isUserAuthenticated = this.authService.isAuthenticated();
+}
--- src/app/guards/anonymous.guard.ts
+++ src/app/guards/anonymous.guard.ts
@@ -1,5 +1,5 @@
 import { CanActivateFn } from '@angular/router';
 
 export const anonymousGuard: CanActivateFn = (route, state) => {
-    return false;
+    return true;
 };
